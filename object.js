5cb535bcd1f7b2c773f640255099616d428f2c35
kieranwatt <kieranmwatt@gmail.com>
2023-10-12 09:26:19 -0400

    thursdayv1

--- object.js
+++ object.js
@@ -3,7 +3,35 @@ const Namesthatrhymewithorange = [
         firstname: "Corange",
         lastname: "Porange",
         age: 12,
-        city: "cleveland"
-
-    }
-]
+        city: "Cleveland",
+        hometown: "Bed-Stuy, NY",
+    },
+    {
+        firstname: "Pyeorange",
+        lastname: "Smorange",
+        age: 93,
+        city: "Oakland",
+        hometown: "Portland, ME",
+    },
+    {
+        firstname: "Torange",
+        lastname: "Morange",
+        age: 31,
+        city: "Dallas",
+        hometown: "Orlando, FL",
+    },
+    {
+        firstname: "OrangeOrange",
+        lastname: "OrangeOrange",
+        age: 2, 
+        city: "Macau",
+        hometown: "Hong-Kong, SAR"
+    },
+    {
+        firstname: "Jorange",
+        lastname: "Gregorius-Oranges",
+        age: 5,
+        city: "Quito",
+        hometown: "Guernsey, UK"
+    },
+];
